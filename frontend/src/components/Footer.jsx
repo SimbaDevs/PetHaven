a84fde0b050a5b476fb49411496035ebf6544065
symonmuchemi <simonkenya846@gmail.com>
2024-06-02 16:33:04 +0300

    footer done

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,24 +6,30 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
+        <div className="socials">
+          <h3>Pet Haven</h3>
+          <div className="images">
+            <img src="facebook.png" alt="facebook" />
+            <img src="gmail.png" alt="twitter" />
+            <img src="phone.png" alt="instagram" />
+          </div>
+        </div>
         <div className="about">
           <h3>About us</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-
-          <div className="socials">
-            <image src="facebook.png" alt="facebook" />
-            <image src="gmail.png" alt="twitter" />
-            <image src="telephone.png" alt="instagram" />
-          </div>
         </div>
         <div className="contact">
           <h3>Contact us</h3>
-          <p>Email: pethaven@gmail.com</p>
-          <p>Phone: +254 712 456 789</p>
+          <p>Email</p>
+          <p>pethaven@gmail.com</p>
+          <p>Phone</p>
+          <p>+254 712 456 789</p>
         </div>
         <div className="location">
           <h3>Location</h3>
-          <p>123 Pet Street, Pet City, PA 12345</p>
+          <p>Biashara street</p>
+          <p>Halison Plaza - second floor</p>
+          <p>PA 12345</p>
         </div>
       </div>
     </footer>
