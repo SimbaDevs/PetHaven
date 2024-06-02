034b200d16370eba7c7b4cefb58dfcdeab5510ae
symonmuchemi <simonkenya846@gmail.com>
2024-06-02 21:00:26 +0300

    added models, schema and view function to fetch all pets

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,20 @@
 from flask import Flask, jsonify, request
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
-import os
+from flask_sqlalchemy import SQLAlchemy # type: ignore
+from flask_marshmallow import Marshmallow # type: ignore
+from schema import PetSchema
+from models import Pet
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pets.db'
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
+
+@app.route('/api/pets', methods=['GET'])
+def pets():
+    pets = Pet.query.all()
+    pet_schema = PetSchema(many=True)
+    return jsonify(pet_schema.dump(pets))
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ backend/schema.py
@@ -0,0 +1,26 @@
+from flask_marshmallow import Marshmallow
+from app import ma
+from models import Pet, Shelter, Vaccine
+
+class VaccineSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Vaccine
+        include_fk = True
+
+class PetSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Pet
+        load_instance = True
+
+    vaccines = ma.Nested(VaccineSchema, many=True)
+    image_url = ma.Method("get_image_url")
+
+    def get_image_url(self, obj):
+        return f"http://localhost:5000/static/images/{obj.name}.jpg"
+
+class ShelterSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Shelter
+        load_instance = True
+
+    pets = ma.Nested(PetSchema, many=True)
