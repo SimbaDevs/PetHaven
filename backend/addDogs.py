28d357516f834f81a48311addb57eae044576a12
symonmuchemi <simonkenya846@gmail.com>
2024-06-03 03:42:53 +0300

    no dir looping

--- backend/addDogs.py
+++ backend/addDogs.py
@@ -7,26 +7,13 @@ app.app_context().push()
 
 faker = Faker()
 
-import os
+dogs = ['morris.jpg', 'Peaches.jpg', 'Martin.jpg', 'Bolt.jpg',
+        'Princess.jpg', 'Rex.jpg', 'Cleopatra.jpg',
+        'Rufus.jpg', 'Sparkles.jpg', 'Russia.jpg',
+        'Lewis.jpg', 'Monty-the-pup.jpg']
 
-# Specify the directory path
-directory_path = "static/images/dogs"
 
-# Create an empty list to store file names
-file_names = []
-
-# Loop through all files in the directory
-for filename in os.listdir(directory_path):
-    # Get the full file path
-    file_path = os.path.join(directory_path, filename)
-    
-    # Check if it's a file (not a directory)
-    if os.path.isfile(file_path):
-        # Add the file name to the list
-        file_names.append(filename)
-
-# Print the list of file names
-dogs = file_names
+print(dogs)
 
 # list of dog breeds
 dog_breeds = [
@@ -37,12 +24,12 @@ dog_breeds = [
 "Miniature Schnauzer", "Bernese Mountain Dog", "Cavalier King Charles Spaniel"
 ]
 
-def add_random_cats():
+def add_random_dogs():
     for dog_image in dogs:
         name = dog_image.split('.')[0]  # Extract name from image file name
         breed = choice(dog_breeds)  # Randomly select a breed
         pet = Pet(
-            pet_type="dog",
+            pet_type="Dogs",
             name=name,
             breed=breed,
             weight=round(randint(3, 8) + faker.random.random(), 1),  # Weight between 3 and 8 kg
@@ -51,7 +38,7 @@ def add_random_cats():
             arrival_date=date.today() - timedelta(days=randint(0, 365)),  # Random arrival date within the last year
             adoption_fee=round(randint(20, 100) + faker.random.random(), 2),  # Adoption fee between $20 and $100
             shelter_id=1,  # Assuming all pets are in shelter with id=1
-            image_url=f"http://localhost:5000/static/images/dogs/{dog_image}"
+            image_url=f"http://localhost:5000/static/images/{dog_image}"
         )
         db.session.add(pet)
         db.session.commit()
@@ -59,4 +46,4 @@ def add_random_cats():
 
 
 if __name__ == "__main__":
-    add_random_cats()
+    add_random_dogs()
