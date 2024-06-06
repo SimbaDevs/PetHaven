27c215f4fe60796fbb8791c73916d2868f7fdad3
symonmuchemi <simonkenya846@gmail.com>
2024-06-07 01:45:19 +0300

    chore: Refactor App.js and add PetDetails component
    only the navbar and the footer are common in all pages.
    specified this on in the element of the route to the landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ import Footer from './components/Footer';
 
 function App() {
     const [selectedOption, setSelectedOption] = useState('all');
-    const [searchQuery, setSearchQuery] = useState('')
+    const [searchQuery, setSearchQuery] = useState('');
 
     const handleOptionChange = (event) => {
         setSelectedOption(event.target.value);
@@ -23,21 +23,24 @@ function App() {
         <div className="App">
             <Router>
                 <Navbar />
-                <header className="App-header">
-                    <h1>Every Pet Deserves a Loving Home.<br /> <span className="highlight">Adopt</span> a Pet Today</h1>
-                    <p>Browse our available animals and learn more about the adoption process. Together, we can <span class="dark-highlight">rescue, rehabilitate, and rehome</span> pets in need.</p>
-                    <select value={selectedOption} onChange={handleOptionChange}>
-                        <option value="all">All</option>
-                        <option value="cat">Cats</option>
-                        <option value="dog">Dogs</option>
-                    </select>
-                    <input type="text" placeholder="Search pet by bread..." value={searchQuery} onChange={handleSearchChange} />
-                    <button className="search-btn">Search</button>
-                </header>
-                <PetList selectedOption={selectedOption} searchQuery={searchQuery} />
                 <Routes>
-                    <Route path="/" exact component={PetList} />
-                    <Route path="/pet/:id" component={PetDetails} />
+                    <Route path="/" element={
+                        <div>
+                            <header className="App-header">
+                                <h1>Every Pet Deserves a Loving Home.<br /> <span className="highlight">Adopt</span> a Pet Today</h1>
+                                <p>Browse our available animals and learn more about the adoption process. Together, we can <span className="dark-highlight">rescue, rehabilitate, and rehome</span> pets in need.</p>
+                                <select value={selectedOption} onChange={handleOptionChange}>
+                                    <option value="all">All</option>
+                                    <option value="cat">Cats</option>
+                                    <option value="dog">Dogs</option>
+                                </select>
+                                <input type="text" placeholder="Search pet by breed..." value={searchQuery} onChange={handleSearchChange} />
+                                <button className="search-btn">Search</button>
+                            </header>
+                            <PetList selectedOption={selectedOption} searchQuery={searchQuery} />
+                        </div>
+                    } />
+                    <Route path="/pet/:id" element={<PetDetails />} />
                 </Routes>
                 <Footer />
             </Router>
