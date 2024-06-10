c069b69b8338b3d130adc416c21088b13217dba1
SymonMuchemi <simonkenya846@gmail.com>
2024-06-10 05:04:33 +0300

    chore: Configure email settings for sending adoption confirmation

--- backend/app.py
+++ backend/app.py
@@ -13,6 +13,14 @@ app.config['SQLALCHEMY_DATABASE_URI'] =\
 'sqlite:///' + os.path.join(basedir, 'pets-data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587 
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = 'muchemi.developer@gmail.com'  # Replace with your email
+app.config['MAIL_PASSWORD'] = os.environ['MAIL_PASSWORD'] # Replace with your email password
+app.config['MAIL_DEFAULT_SENDER'] = 'muchemi.developer@gmail.com'  # Replace with your email
+
 
 db = SQLAlchemy(app)
 
--- backend/routes.py
+++ backend/routes.py
@@ -1,11 +1,40 @@
 from app import app, Pet, db
 from flask import jsonify, request
 from schema import PetSchema
-from flask_cors import CORS
+from flask_cors import CORS # type: ignore
 from app import AdoptionFormSubmission
+from flask_mail import Mail, Message # type: ignore
+
 
 # Enable CORS
 CORS(app)
+
+mail = Mail(app)
+
+def send_confirmation_email(recipient, pet):
+    msg = Message('Adoption Confirmation', recipients=[recipient])
+    msg.body = f"""
+    Dear {recipient},
+
+    Thank you for your interest in adopting {pet.name}. Here are the details of the pet:
+
+    Name: {pet.name}
+    Breed: {pet.breed}
+    Age: {pet.age}
+    Location: {pet.location}
+    Adoption Fee: {pet.adoption_fee}
+
+    We will contact you shortly with more details.
+
+    Best regards,
+    Pet Haven
+    """
+
+    # # Attach the pet's image if available
+    # if pet.image_url:
+    #     # assuming pet.image_url is something like 'static/images/pet_name.jpg'
+    #     with app.open_resource(pet.image_url) as fp:
+    #         msg.attach(f"/static/images/{pet.name}.jpg", "image/jpeg", fp.read())
 
 
 # get all pets
@@ -27,6 +56,10 @@ def get_pet(id):
 @app.route('/api/v1/adopt', methods=['POST'])
 def adopt():
     data = request.get_json()
+    pet = Pet.query.get(data['pet_id'])
+    if not pet:
+        return jsonify({'message': 'Pet not found!'}), 404
+
     new_adoption = AdoptionFormSubmission(
         first_name=data['first_name'],
         last_name=data['last_name'],
@@ -37,6 +70,9 @@ def adopt():
     )
     db.session.add(new_adoption)
     db.session.commit()
+    
+    send_confirmation_email(data['email'], pet)
+    
     return jsonify({'message': 'Adoption form submitted successfully!'}), 201
 
 
