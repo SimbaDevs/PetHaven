ce2b645335a3b9f2284da3dce4a4252d03b587a3
symonmuchemi <simonkenya846@gmail.com>
2024-06-14 15:07:33 +0300

    ui-update: added styling to SignIn component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,6 +24,7 @@ function App() {
         <div className="App">
             <Router>
                 <Routes>
+                    <Route path="/signin" element={<SignIn />} />
                     <Route path="/" element={
                         <div>
                             <Navbar />
--- frontend/src/components/SignIn.jsx
+++ frontend/src/components/SignIn.jsx
@@ -1,22 +1,45 @@
 import React from "react";
+import "./styles/SignIn.css";
+import { useNavigate } from "react-router-dom";
 
 function SignIn() {
+  const navigate = useNavigate();
+
   return (
     <div className="auth-container">
       <div className="form-image">
         <img src="auth-page-photo.png" alt="dog with owner" />
       </div>
       <div className="form-container">
-        <h1>Sign in</h1>
+        <div className="headers">
+          <p className="main">Sign In</p>
+          <p className="sub">Sign Up</p>
+        </div>
         <form action="">
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" name="email" />
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" name="password" />
-          <a href="#" className="forgot-pws">
+          <div className="form-input">
+            <input type="email" id="email" name="email" placeholder="Email" />
+          </div>
+          <div className="form-input">
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+            />
+          </div>
+          <a href={navigate()} className="forgot-pwd">
             Forgot password?
           </a>
           <button type="submit">Sign in</button>
+          <div className="divider-container">
+            <div className="divider-line"></div>
+            <span className="divider-text">or continue with</span>
+            <div className="divider-line"></div>
+          </div>
+          <div className="google-login">
+            <img src="google-icon.png" alt="google icon" />
+            <span>Sign in with Google</span>
+          </div>
         </form>
       </div>
     </div>
