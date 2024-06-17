b80e0e0e452a6c246e7e9403d859807c14b67e28
symonmuchemi <simonkenya846@gmail.com>
2024-06-17 22:35:23 +0300

    feat: Add homepage with header and pet list
    This seperates the conditions where the navbar and footer are not needed.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import './App.css';
 import React, { useState } from 'react';
 import PetDetails from './components/PetDetails';
@@ -8,7 +8,7 @@ import Footer from './components/Footer';
 import AdoptionForm from './components/AdoptForm';
 import SignIn from './components/SignIn';
 
-function App() {
+function HomePage() {
     const [selectedOption, setSelectedOption] = useState('all');
     const [searchQuery, setSearchQuery] = useState('');
 
@@ -19,37 +19,48 @@ function App() {
     const handleSearchChange = (event) => {
         setSearchQuery(event.target.value);
     };
+    return (
+        <div>
+            <header className="App-header">
+                <h1>Every Pet Deserves a Loving Home.<br /> <span className="highlight">Adopt</span> a Pet Today</h1>
+                <p>Browse our available animals and learn more about the adoption process. Together, we can <span className="dark-highlight">rescue, rehabilitate, and rehome</span> pets in need.</p>
+                <select value={selectedOption} onChange={handleOptionChange}>
+                    <option value="all">All</option>
+                    <option value="Cats">Cats</option>
+                    <option value="Dogs">Dogs</option>
+                </select>
+                <input type="text" placeholder="Search pet by breed..." value={searchQuery} onChange={handleSearchChange} />
+                <button className="search-btn">Search</button>
+            </header>
+            <PetList selectedOption={selectedOption} searchQuery={searchQuery} />
+        </div>
+    )
+}
+
+function App() {
+    return (
+        <Router>
+            <PageLayout />
+        </Router>
+    )
+}
+
+function PageLayout() {
+    let location = useLocation()
+    let showNavAndFooter = location.pathname !== '/signin';
 
     return (
-        <div className="App">
-            <Navbar />
-            <Router>
-                <Navbar />
-                <Routes>
-                    <Route path="/" element={
-                        <div>
-                            <header className="App-header">
-                                <h1>Every Pet Deserves a Loving Home.<br /> <span className="highlight">Adopt</span> a Pet Today</h1>
-                                <p>Browse our available animals and learn more about the adoption process. Together, we can <span className="dark-highlight">rescue, rehabilitate, and rehome</span> pets in need.</p>
-                                <select value={selectedOption} onChange={handleOptionChange}>
-                                    <option value="all">All</option>
-                                    <option value="Cats">Cats</option>
-                                    <option value="Dogs">Dogs</option>
-                                </select>
-                                <input type="text" placeholder="Search pet by breed..." value={searchQuery} onChange={handleSearchChange} />
-                                <button className="search-btn">Search</button>
-                            </header>
-                            <PetList selectedOption={selectedOption} searchQuery={searchQuery} />
-                        </div>
-                    } />
-                    <Route path="/signin" element={<SignIn />} />
-                    <Route path="/pet/:id" element={<PetDetails />} />
-                    <Route path="/adopt/:id" element={<AdoptionForm />} />
-                </Routes>
-                <Footer />
-            </Router>
+        <div className='App'>
+            {showNavAndFooter && <Navbar />}
+            <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='/signin' element={<SignIn />} />
+                <Route path="/pet/:id" element={<PetDetails />} />
+                <Route path="/adopt/:id" element={<AdoptionForm />} />
+            </Routes>
+            {showNavAndFooter && <Footer />}
         </div>
-    );
+    )
 }
 
 export default App;
